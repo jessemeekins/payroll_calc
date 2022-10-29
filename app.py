a88432176a89b39a05889e55e8a74e0dbc41be82
jessemeekins <87320459+jessemeekins@users.noreply.github.com>
2022-10-28 20:05:15 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -29,134 +29,150 @@ with st.container():
 
 
 
-    # Defining from the db the rank
-    selected_rank = st.selectbox('Select Rank', data['Rank'])
-    # Defining the pay selector and option "24 hours Rate" or "8 hours rate"
-    selected_pay_rate = st.selectbox('Hourly Pay Rate', ['24 Hour Pay', '8 Hour Pay'])
-    # Defining the pay selector and option "Time and A Half" or "Straight Time"
-    overtime_type = st.selectbox('Type of Pay', ['Time and a Half', 'Straight Time'])
-    # Text input box for mannually entering floats
-    longevity = st.number_input(
-        'Longevity Incentive.', 
-        min_value=0.0,
-        max_value=1.9999,
-        step=1e-4,
-        format="%.4f")
-    # Text input box for mannually entering floats
-    job_incentive = st.number_input(
-        'Insert Job Incentive Pay', 
-        min_value=0.0,
-        max_value=1.9999,
-        step=1e-4,
-        format="%.4f")
-    # Text input box for mannually entering floats
-    college_inc = st.number_input(
-        'Insert College Incentive', 
-        min_value=0.0,
-        max_value=1.9999,
-        step=1e-4,
-        format="%.4f")
-    # Text inout how many hours they work
-    hours_works = st.number_input('Hours Worked (Decimal Value)',
-        step=25e-2,
+# Defining from the data the rank
+selected_rank = st.selectbox('Select Rank', data['Rank'])
+# Defining the pay selector and option "24 hours Rate" or "8 hours rate"
+selected_pay_rate = st.selectbox('Hourly Pay Rate', ['24 Hour Pay', '8 Hour Pay'])
+# Defining the pay selector and option "Time and A Half" or "Straight Time"
+overtime_type = st.selectbox('Type of Pay', ['Time and a Half', 'Straight Time'])
+# Text input box for mannually entering floats
+longevity = st.number_input(
+    'Longevity Incentive.', 
+    min_value=0.0,
+    max_value=1.9999,
+    step=1e-4,
+    format="%.4f")
+# Text input box for mannually entering floats
+job_incentive = st.number_input(
+    'Insert Job Incentive Pay', 
+    min_value=0.0,
+    max_value=1.9999,
+    step=1e-4,
+    format="%.4f")
+# Text input box for mannually entering floats
+college_inc = st.number_input(
+    'Insert College Incentive', 
+    min_value=0.0,
+    max_value=1.9999,
+    step=1e-4,
+    format="%.4f")
+# Text inout how many hours they work
+hours_works = st.number_input('Hours Worked (Decimal Value)',
+    # Defines incriments of chage too 25 on the second decimal value
+    step=25e-2,
     format="%.2f")
 
 
 
-    hourly_pay = data[data['Rank'] == selected_rank]
-    hourly_pay = hourly_pay[selected_pay_rate].values
-    hourly_pay = hourly_pay[0]
+hourly_pay = data[data['Rank'] == selected_rank]
+hourly_pay = hourly_pay[selected_pay_rate].values
+hourly_pay = hourly_pay[0]
 
-    def determine_time_and_half(overtime_type=overtime_type):
-        if overtime_type == 'Time and a Half': 
-            overtime_rate = 1.5
-        else:
-            overtime_rate = 1.0
-        return overtime_rate
+def determine_time_and_half(overtime_type=overtime_type):
+    if overtime_type == 'Time and a Half': 
+        overtime_rate = 1.5
+    else:
+        overtime_rate = 1.0
+    return overtime_rate
 
-    hourly_rate = determine_time_and_half()
+hourly_rate = determine_time_and_half()
 
-    def longevity_inc():
-        return round(longevity*hours_works*hourly_rate, 2)
+def longevity_inc():
+    return round(longevity*hours_works*hourly_rate, 2)
 
-    def job_inc():
-        return round(job_incentive*hours_works*hourly_rate, 2)
+def job_inc():
+    return round(job_incentive*hours_works*hourly_rate, 2)
 
-    def coll_inc():
-        return round(college_inc*hours_works*hourly_rate, 2)
-
-
-        
+def coll_inc():
+    return round(college_inc*hours_works*hourly_rate, 2)
 
 
-    longevity = longevity_inc()
-    job = job_inc()
-    coll = coll_inc()
+    
 
-    incent = round(longevity+job+coll, 2)
-    calc_pay = round(hours_works*hourly_pay*hourly_rate, 4)
 
-    total_pay = round(incent + calc_pay, 2)
+longevity = longevity_inc()
+job = job_inc()
+coll = coll_inc()
 
-    name = st.text_input('Add ID or Name (optional)')
-    '---'
-   
-    col1, col2, col3 = st.columns(3)
+incent = round(longevity+job+coll, 2)
+calc_pay = round(hours_works*hourly_pay*hourly_rate, 4)
 
-    col1.metric(' Selected Rank', value=selected_rank)
-    col2.metric('Base Pay', value=f'${hourly_pay}')
-    col3.metric('Hours', value=hours_works)
-    col1.metric('Total Incentive Pay', value=f'${incent}')
-    col2.metric('Pay w/o Incentives', value=f'${calc_pay}')
-    col3.metric(f' Total Pay', value=f'${total_pay}')
+total_pay = round(incent + calc_pay, 2)
 
-if "key" not in st.session_state:
-    st.session_state.key = pd.DataFrame(columns=[
-            'Name',
-            'Rank', 
-            'Hourly Pay', 
-            'Pay Type', 
-            'Hours Worked', 
-            'Longevity Incent', 
-            'Job Incent', 
-            'College Incent',
-            'All Incentives',
-            'Pay w/o Incent', 
-            'Total Pay'
-    ], index=[0])
-
-new_df = pd.DataFrame({
-        'Name': name, 
-        'Rank': selected_rank, 
-        'Hourly Pay': hourly_pay, 
-        'Pay Type': hourly_rate, 
-        'Hours Worked': hours_works, 
-        'Longevity Incent': longevity, 
-        'Job Incent': job, 
-        'College Incent': coll,
-        'All Incentives': incent,
-        'Pay w/o Incent': calc_pay, 
-        'Total Pay': total_pay
-        }, index=[0])
-
-run = st.button('Calculate/Add to Session List')
+name = st.text_input('Add ID or Name (optional)')
 '---'
 
+col1, col2, col3 = st.columns(3)
+
+col1.metric(' Selected Rank', value=selected_rank)
+col2.metric('Base Pay', value=f'${hourly_pay}')
+col3.metric('Hours', value=hours_works)
+col1.metric('Total Incentive Pay', value=f'${incent}')
+col2.metric('Pay w/o Incentives', value=f'${calc_pay}')
+col3.metric(f' Total Pay', value=f'${total_pay}')
+
+
+
+# Initalize "key" for session state
+if "key" not in st.session_state:
+        st.session_state.key = pd.DataFrame(columns=[
+                'Name',
+                'Rank', 
+                'Hourly Pay', 
+                'Pay Type',
+                'Hours Worked', 
+                '24 or 8', 
+                'Longevity Incent', 
+                'Job Incent', 
+                'College Incent',
+                'All Incentives',
+                'Pay w/o Incent', 
+                'Total Pay'
+        ], index=[0])
+
+
+
+# "run" varoable create to trigger sessio.state.key
+run = st.button('Calculate/Add to Session List')
+
+'---'
+# if above button is clicked, then run is True and condidtional statement executes
 if run:
+    # Passing above values to a new data frame
+    new_df = pd.DataFrame({
+            'Name': name, 
+            'Rank': selected_rank, 
+            'Hourly Pay': hourly_pay, 
+            'Pay Type': hourly_rate,
+            '24 or 8': selected_pay_rate, 
+            'Hours Worked': hours_works, 
+            'Longevity Incent': longevity, 
+            'Job Incent': job, 
+            'College Incent': coll,
+            'All Incentives': incent,
+            'Pay w/o Incent': calc_pay, 
+            'Total Pay': total_pay
+            }, index=[0])
+    # concartinating the new information to a new dataframe, passing it to the session_state.key
     st.session_state.key = pd.concat([st.session_state.key, new_df], axis=0)
+    # Calling on new session_state.key variable to display on the screen 
     st.dataframe(st.session_state.key)
-st.write('*Table disapears when altering the next set of paramaters')
+# Informing of some functionality glitch that makes df disapear when altering new data fields
+st.write('*Table disapears when altering the next set of paramaters, but keeps data.')
+# Counter to calm the nerves of rthe user to make sure that they dont think they lost the session data
 st.write(f"Total Rows: {st.session_state.key.shape[0]}")
 
 '---'
 
+
+# Download data to excel file, still working out compatability issues on streamlit
 #file_name = st.text_input('file Name (optional)') 
 
-#def save_download_sessiom():
-#    df = st.session_state.key
-#    if file_name == '':
-#        df.to_excel('session.xlsx')
-#    else:
-#        df.to_excel(f'{file_name}.xlsx')
+#    def save_download_sessiom():
+#       df = st.session_state.key
+#       if file_name == '':
+#           df.to_excel('session.xlsx')
+#       else:
+#           df.to_excel(f'{file_name}.xlsx')
 #
-#st.button('Save Session', on_click=save_download_sessiom())'''
+#    st.button('Save Session', on_click=save_download_sessiom())'''
